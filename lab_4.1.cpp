12500f02767ff767a6ec38de1a62d142a05e7eeb
denysbandrivskyi <denys.bandrivskyi.ri.2023@lpnu.ua>
2023-10-12 03:38:36 +0300

    fixed

---
+++ lab_4.1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k, N;
+    double result = 0.0;
+
+    cout << "Enter the lower bound (k): ";
+    cin >> k;
+
+    cout << "Enter the upper bound (N): ";
+    cin >> N;
+
+    // Using while loop
+    int i = k;
+    while (i <= N) {
+        result += (1.0 * i * i) / (k * k + N * N);
+        i++;
+    }
+
+    cout << "Sum using while loop: " << result << endl;
+
+    result = 0.0; // Reset result
+
+    // Using do-while loop
+    i = k;
+    do {
+        result += (1.0 * i * i) / (k * k + N * N);
+        i++;
+    } while (i <= N);
+
+    cout << "Sum using do-while loop: " << result << endl;
+
+    result = 0.0; // Reset result
+
+    // Using for loop (increasing)
+    for (i = k; i <= N; i++) {
+        result += (1.0 * i * i) / (k * k + N * N);
+    }
+
+    cout << "Sum using for loop (increasing): " << result << endl;
+
+    result = 0.0; // Reset result
+
+    // Using for loop (decreasing)
+    for (i = N; i >= k; i--) {
+        result += (1.0 * i * i) / (k * k + N * N);
+    }
+
+    cout << "Sum using for loop (decreasing): " << result << endl;
+
+    return 0;
+}
